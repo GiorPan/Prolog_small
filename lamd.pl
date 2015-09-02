89d5c7b57b68778c0d3efc9334aa3fafd19eadd9
GiorPan <panagiwtopoulos.giwrgos@gmail.com>
2015-09-02 11:46:49 +0300

    Create lamd.pl

---
+++ lamd.pl
@@ -0,0 +1,10 @@
+/* give a list of ints as input and check if their values first rise and then fall 
+example run "lambda([1,4,3,2,1])-->True, lambda([1,4,3,2,4])-->False"*/
+
+lambda([X,Y,Z]):- !,X<Y , Y>Z.
+lambda([X,Y|L]):- X<Y ,up([Y|L]),!.
+up([X,Y|L]):- X<Y , up([Y|L]).
+up([X,Y|L]):- X>Y ,down([X,Y|L]).
+down([]).
+down([_]).
+down([X,Y|L]):- X>Y, down([Y|L]).
